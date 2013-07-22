82fbeee04bab592da723bf0e99f31b07641a2939
viticm <viticm@126.com>
2013-07-22 22:04:58 +0800

    Revert "fix some bug"
    This reverts commit 57a8ee16dbea4b2e9783d7f742f8e21264dba788.

--- server/Server/Main/ThreadManager.cpp
+++ server/Server/Main/ThreadManager.cpp
@@ -78,20 +78,19 @@ __ENTER_FUNCTION
         pSceneThread = new SceneThread ;
         Assert( pSceneThread ) ;
 
+        LERR( "FILE: %s, i: %d", __FILE__, i )
         ret = m_pThreadPool->AddThread( i, pSceneThread ) ;
         Assert( ret ) ;
 
         m_nThreads ++ ;
     }
 
-    /**
     for ( i = 0; i < uMaxThreadCount; i++ )
     {
         Thread** m_pThread = m_pThreadPool->GetThread();
         SceneThread* pSceneThread1 = (SceneThread*)( m_pThread[ i ] );
         LERR( "uThreadIndex: %d, obj: %d , sizeof( pSceneThread1 ):%d", i, pSceneThread1, sizeof( pSceneThread1 ) );
     }
-    **/ //my debug
 
     for( i=0; i<count; i++ )
     {
@@ -109,6 +108,7 @@ __ENTER_FUNCTION
             continue ;
         }
 
+        LERR( "FILE: %s, LINE: %d, m_ThreadIndex: %d", __FILE__, __LINE__, g_Config.m_SceneInfo.m_pScene[i].m_ThreadIndex );
         SceneThread* pSceneThread = (SceneThread*)(m_pThreadPool->GetThreadByIndex(g_Config.m_SceneInfo.m_pScene[i].m_ThreadIndex)) ;
         if( pSceneThread==NULL )
         {
@@ -120,9 +120,11 @@ __ENTER_FUNCTION
             Scene* pScene = g_pSceneManager->GetScene(SceneID) ;
             pSceneThread->AddScene( pScene );
 
+    LERR( "FILE: %s, LINE: %d", __FILE__, __LINE__ );
         }
     }
 
+    LERR( "FILE: %s, LINE: %d", __FILE__, __LINE__ );
     if( m_pServerThread->IsActive() )
     {
         m_nThreads ++ ;
