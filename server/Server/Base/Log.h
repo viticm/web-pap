1838c0abc23ee3585731a3cfcb67840c572ecb1f
viticm <viticm@126.com>
2013-08-13 22:27:17 +0800

    add some instrations

--- server/Server/Base/Log.h
+++ server/Server/Base/Log.h
@@ -1,3 +1,13 @@
+/**
+ * PAP Server Engine ( https://github.com/viticm/web-pap )
+ * $Id Log.h
+ * @link https://github.com/viticm/web-pap/tree/master/server for the canonical source repository
+ * @copyright Copyright (c) 2013-2013 viticm( viticm@126.com )
+ * @license
+ * @user viticm<viticm@126.com>
+ * @date 2013-8-13 22:25:19
+ * @uses the logs module
+ */
 
 #ifndef __LOG_H__
 #define __LOG_H__
--- server/Server/Base/TimeManager.h
+++ server/Server/Base/TimeManager.h
@@ -1,3 +1,13 @@
+/**
+ * PAP Server Engine ( https://github.com/viticm/web-pap )
+ * $Id TimeManager.h
+ * @link https://github.com/viticm/web-pap/tree/master/server for the canonical source repository
+ * @copyright Copyright (c) 2013-2013 viticm( viticm@126.com )
+ * @license
+ * @user viticm<viticm@126.com>
+ * @date 2013-8-13 22:25:19
+ * @uses the time manager module
+ */
 
 #ifndef __TIMEMANAGER_H__
 #define __TIMEMANAGER_H__
