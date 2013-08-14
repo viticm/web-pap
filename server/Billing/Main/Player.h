3c3e0b782b5d431add598ab6c8c87e5578eab74f
viticm <viticm@126.com>
2013-08-14 20:12:21 +0800

    change the code style for mine

--- server/Billing/Main/Player.h
+++ server/Billing/Main/Player.h
@@ -1,9 +1,13 @@
-//
-// Player 是指已经通过网络连接进入服务器端程序的客户
-//
-//
-//
-
+/**
+ * PAP Server Engine ( https://github.com/viticm/web-pap )
+ * $Id Player.h
+ * @link https://github.com/viticm/web-pap/tree/master/server for the canonical source repository
+ * @copyright Copyright (c) 2013-2013 viticm( viticm@126.com )
+ * @license
+ * @user viticm<viticm@126.com>
+ * @date 2013-8-14 19:45:46
+ * @uses the player module of the connect net user
+ */
 #ifndef __PLAYER_H__
 #define __PLAYER_H__
 
@@ -20,7 +24,7 @@
 //如果在一定时间内没有收到任何消息，则断开此客户端的网络连接
 #define MAX_KICK_TIME 6000000
 
-
+// 该模块在所有服务器中都存在，可以考虑公用，不过需考虑共用性
 enum PACKET_FLAG
 {
     PF_NONE=0 ,
@@ -30,34 +34,34 @@ enum PACKET_FLAG
 struct ASYNC_PACKET
 {
     Packet*        m_pPacket ;
-    PlayerID_t    m_PlayerID ;
-    UINT        m_Flag ;
+    PlayerID_t     m_PlayerID ;
+    UINT           m_Flag ;
 
-    ASYNC_PACKET( )
+    ASYNC_PACKET()
     {
-        m_pPacket = NULL ;
-        m_PlayerID = INVALID_ID ;
-        m_Flag = PF_NONE ;
-    };
-    ~ASYNC_PACKET( )
+        m_pPacket     = NULL ;
+        m_PlayerID    = INVALID_ID ;
+        m_Flag        = PF_NONE ;
+    } ;
+    ~ASYNC_PACKET()
     {
         SAFE_DELETE( m_pPacket ) ;
         m_PlayerID = INVALID_ID ;
-        m_Flag = PF_NONE ;
-    };
-};
+        m_Flag     = PF_NONE ;
+    } ;
+} ;
 
 
 
 class Player
 {
 public :
-    Player( ) ;
-    ~Player( ) ;
+    Player() ;
+    ~Player() ;
 
-    virtual BOOL    ProcessInput( ) ;
-    virtual BOOL    ProcessOutput( ) ;
-    virtual BOOL    ProcessCommand( bool Option = true ) ;
+    virtual BOOL    ProcessInput() ;
+    virtual BOOL    ProcessOutput() ;
+    virtual BOOL    ProcessCommand( BOOL Option = true ) ;
 
     virtual BOOL    HeartBeat( DWORD dwTime=0 ) ;
 
@@ -67,13 +71,13 @@ public :
 public :
     //通用接口
 
-    virtual BOOL    IsGamePlayer( )=0 ;
-    virtual BOOL    IsServerPlayer( )=0 ;
+    virtual BOOL    IsGamePlayer() = 0 ;
+    virtual BOOL    IsServerPlayer() = 0 ;
 
     //读取设置玩家PID的接口，PlayerID 是由PlayerPool类分配出来的索引值
     //PlayerID用于标识一个客户端连接在整个系统中的数据位置信息
     PlayerID_t    PlayerID(){ return m_PID ; } ;
-    void        SetPlayerID( PlayerID_t id ){ m_PID = id ; } ;
+    VOID          SetPlayerID( PlayerID_t id ){ m_PID = id ; } ;
 
     //读取设置玩家UID的接口，UserID 是由UserPool类分配出来的索引值
     //UserID用于标识玩家的游戏存储位置信息
@@ -88,26 +92,26 @@ public :
 
     //读取当前玩家的Socket类
     //网络连接接口
-    Socket*        GetSocket(){ return m_pSocket ; } ;
+    Socket*     GetSocket(){ return m_pSocket ; } ;
 
     //断开与当前玩家的网络连接
-    virtual void            Disconnect( ) ;
+    virtual    VOID            Disconnect() ;
 
     //判断当前玩家的网络连接是否有效
-    virtual    BOOL            IsValid( ) ; 
+    virtual    BOOL            IsValid() ; 
 
     //清除当前玩家网络连接数据和缓存数据
-    virtual    void            CleanUp( ) ;
+    virtual    VOID            CleanUp() ;
 
     //判断当前玩家数据块是否是空块；
     //如果是空块，则可以被PlayerPool分配出去用于接收新连接
-    BOOL                    IsEmpty( ) { return m_IsEmpty ; }
-    void                    SetEmpty( BOOL opt = TRUE ) { m_IsEmpty = opt ; }
+    BOOL                    IsEmpty() { return m_IsEmpty ; }
+    VOID                    SetEmpty( BOOL opt = TRUE ) { m_IsEmpty = opt ; }
 
-    BOOL                    IsDisconnect( ) { return m_IsDisconnect ; }
-    void                    SetDisconnect( BOOL opt = TRUE ) { m_IsDisconnect = opt ; }
+    BOOL                    IsDisconnect() { return m_IsDisconnect ; }
+    VOID                    SetDisconnect( BOOL opt = TRUE ) { m_IsDisconnect = opt ; }
 
-    virtual void            ResetKick( ) ;
+    virtual VOID            ResetKick() ;
 
 private :
     BOOL                    m_IsEmpty ;
@@ -117,23 +121,21 @@ private :
 protected :
     //Player ID
     //统一由PlayerPool管理
-    PlayerID_t                m_PID ;    
+    PlayerID_t            m_PID ;    
     //User ID
     //统一由UserPool管理
-    ID_t                    m_UID ;    
+    ID_t                  m_UID ;    
     //在PlayerManager类中player id数组的下标
-    ID_t                    m_PlayerManagerID ;
+    ID_t                  m_PlayerManagerID ;
 
     //网络连接句柄
-    Socket*                    m_pSocket ;
+    Socket*               m_pSocket ;
 
     //输入输出数据缓存
-    SocketInputStream*        m_pSocketInputStream ;
-    SocketOutputStream*        m_pSocketOutputStream ;
+    SocketInputStream*    m_pSocketInputStream ;
+    SocketOutputStream*   m_pSocketOutputStream ;
 
-
-
-    BYTE                    m_PacketIndex ;
+    BYTE                  m_PacketIndex ;
 };
 
 
