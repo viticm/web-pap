0c1ded3d0f22030c6fbb37ee8dea2464fff5ee21
viticm <viticm@126.com>
2013-08-22 14:48:56 +0800

    add some doc for db head file

--- server/Common/DBSystem/DataBase/DBGlobalData.h
+++ server/Common/DBSystem/DataBase/DBGlobalData.h
@@ -1,3 +1,13 @@
+/**
+ * PAP Server Engine ( https://github.com/viticm/web-pap )
+ * $Id SMULogicManager.h
+ * @link https://github.com/viticm/web-pap/tree/master/server for the canonical source repository
+ * @copyright Copyright (c) 2013-2013 viticm( viticm@126.com )
+ * @license
+ * @user viticm<viticm@126.com>
+ * @date 2013-8-22 14:44:42
+ * @uses the global data manager of database 
+ */
 #pragma once
 
 #include "Type.h"
