82c858cca399c7ace4ae9007ecca021bb81e7770
viticm <viticm@126.com>
2013-09-04 11:41:18 +0800

    change the code style for mine

--- server/Login/Main/LoginMain.h
+++ server/Login/Main/LoginMain.h
@@ -1,10 +1,15 @@
+/**
+ * PAP Server Engine ( https://github.com/viticm/web-pap )
+ * $Id LoginMain.h
+ * @link https://github.com/viticm/web-pap/tree/master/server for the canonical source repository
+ * @copyright Copyright (c) 2013-2013 viticm( viticm@126.com )
+ * @license
+ * @user viticm<viticm@126.com>
+ * @date 2013-9-4 10:00:37
+ * @uses the login main file
+ */
 #ifndef _LOGIN_MAIN_H_
 #define _LOGIN_MAIN_H_
 
 #include "Type.h"
-
-
-
-
-
 #endif
