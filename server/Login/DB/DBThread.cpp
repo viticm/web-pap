5608691877c502a43672b4e38e01eef8d0d035e3
viticm <viticm@126.com>
2013-09-04 16:33:49 +0800

    change the code style for mine

--- server/Login/DB/DBThread.cpp
+++ server/Login/DB/DBThread.cpp
@@ -9,100 +9,109 @@
 
 DBThread::DBThread()
 {
-    AutoLock_T autolock(m_Lock);
-    m_Active = TRUE;
-    mCharDBInterface = new ODBCInterface;
+    __ENTER_FUNCTION
+
+        AutoLock_T autolock( m_Lock ) ;
+        m_Active = TRUE ;
+        mCharDBInterface = new ODBCInterface ;
+
+    __LEAVE_FUNCTION
 }
+
 DBThread::~DBThread()
 {
-    AutoLock_T autolock(m_Lock);
-    SAFE_DELETE(mCharDBInterface);
+    __ENTER_FUNCTION
+
+        AutoLock_T autolock( m_Lock ) ;
+        SAFE_DELETE( mCharDBInterface ) ;
+
+    __LEAVE_FUNCTION
 }
 
 BOOL DBThread::Init()
 {
     __ENTER_FUNCTION
 
-    //从Config 读取Login DB 相关的数据
-    CHAR    Host[HOST_STR_LEN];
-    strncpy(Host,g_Config.m_LoginInfo.m_DBIP,HOST_STR_LEN);                        //连接对端IP
-    Host[HOST_STR_LEN-1] = '\0';
+        //从Config 读取Login DB 相关的数据
+        CHAR Host[ HOST_STR_LEN ] ;
+        strncpy( Host, g_Config.m_LoginInfo.m_DBIP, HOST_STR_LEN ) ;                        //连接对端IP
+        Host[ HOST_STR_LEN - 1 ] = '\0' ;
 
-    UINT    Port                            =    g_Config.m_LoginInfo.m_DBPort;    //连接对端端口
-    CHAR    Database[DATABASE_STR_LEN];
-    strncpy(Database,g_Config.m_LoginInfo.m_DBName,DATABASE_STR_LEN);            //数据库名称
-    Database[DATABASE_STR_LEN-1] = '\0';
+        UINT Port = g_Config.m_LoginInfo.m_DBPort ;    //连接对端端口
+        CHAR Database[ DATABASE_STR_LEN ] ;
+        strncpy( Database, g_Config.m_LoginInfo.m_DBName, DATABASE_STR_LEN ) ;            //数据库名称
+        Database[ DATABASE_STR_LEN - 1 ] = '\0' ;
 
-    CHAR    User[DB_USE_STR_LEN];                                                //用户名称
-    strncpy(User,g_Config.m_LoginInfo.m_DBUser,DB_USE_STR_LEN);
-    User[DB_USE_STR_LEN-1] = '\0';
+        CHAR User[ DB_USE_STR_LEN ] ;                                                //用户名称
+        strncpy( User, g_Config.m_LoginInfo.m_DBUser, DB_USE_STR_LEN ) ;
+        User[ DB_USE_STR_LEN - 1 ] = '\0' ;
 
-    CHAR    Password[DB_PASSWORD_STR_LEN];                                        //密码
-    strncpy(Password,g_Config.m_LoginInfo.m_DBPassword,DB_PASSWORD_STR_LEN);
-    Password[DB_PASSWORD_STR_LEN-1] = '\0';
+        CHAR Password[ DB_PASSWORD_STR_LEN ] ;                                        //密码
+        strncpy( Password, g_Config.m_LoginInfo.m_DBPassword, DB_PASSWORD_STR_LEN ) ;
+        Password[ DB_PASSWORD_STR_LEN - 1 ] = '\0' ;
 
-    Assert(mCharDBInterface);
+        Assert( mCharDBInterface ) ;
 
-    mCharDBInterface->Connect(Database,
-        User,
-        Password);
+        mCharDBInterface->Connect( Database, User, Password ) ;
 
-    if(!mCharDBInterface->IsConnected())
-    {
-        Log::SaveLog(LOGIN_LOGFILE,"DBThread:mCharDBInterface->Connect()... Get Errors: %s ",
-            mCharDBInterface->GetErrorMsg());
-    }
-    return mCharDBInterface->IsConnected();
-    return TRUE;
+        if ( !mCharDBInterface->IsConnected() )
+        {
+            Log::SaveLog( LOGIN_LOGFILE, "DBThread:mCharDBInterface->Connect()... Get Errors: %s ",
+                mCharDBInterface->GetErrorMsg() ) ;
+        }
+        
+        return mCharDBInterface->IsConnected() ;
+
     __LEAVE_FUNCTION
-    return FALSE;
+        
+        return FALSE ;
 
 }
 
-ODBCInterface*        DBThread::GetInterface()
+ODBCInterface* DBThread::GetInterface()
 {
-    return mCharDBInterface;
+    return mCharDBInterface ;
 }
 
-BOOL                DBThread::IsConnected()
+BOOL DBThread::IsConnected()
 {
-    return mCharDBInterface->IsConnected();
+    return mCharDBInterface->IsConnected() ;
 }
 
 
-VOID    DBThread::run()
+VOID DBThread::run()
 {
     __ENTER_FUNCTION
 
-            while (m_Active)
+        while ( m_Active )
+        {
+            //Test connection
+            Assert( mCharDBInterface ) ;
+            if ( FALSE == mCharDBInterface->IsConnected() )
             {
-                //Test connection
-                Assert(mCharDBInterface);
-                if(mCharDBInterface->IsConnected() == FALSE)
+                BOOL bRet = mCharDBInterface->Connect() ;
+                if ( !bRet )
                 {
-                    BOOL bRet = mCharDBInterface->Connect();
-                    if(!bRet)
-                    {
-                        Log::SaveLog(LOGIN_LOGFILE,"DBThread::mCharDBInterface->Connect() Get Errors: %s,TID = %d,ErrorCode =%d",
-                            mCharDBInterface->GetErrorMsg(),getTID(),mCharDBInterface->GetError());
-                        MySleep(5000);//保证不频繁连接数据库
-                    }
-                    else
-                        Log::SaveLog(LOGIN_LOGFILE,"DBThread::Reconnect database,TID=%d",getTID());
+                    Log::SaveLog( LOGIN_LOGFILE, "DBThread::mCharDBInterface->Connect() Get Errors: %s, TID = %d, ErrorCode = %d",
+                        mCharDBInterface->GetErrorMsg(),getTID(),mCharDBInterface->GetError() ) ;
+                    MySleep( 5000 ) ;//保证不频繁连接数据库
                 }
-                //execute Commands
-                MySleep(1);
-                BOOL bRet = ProcessCacheCommand();
-                Assert(bRet);
+                else
+                    Log::SaveLog( LOGIN_LOGFILE,"DBThread::Reconnect database,TID = %d", getTID() ) ;
             }
+            //execute Commands
+            MySleep( 1 ) ;
+            BOOL bRet = ProcessCacheCommand() ;
+            Assert( bRet ) ;
+        }
 
     __LEAVE_FUNCTION
-
-
 }
 
-BOOL    DBThread::ProcessCacheCommand()
+BOOL DBThread::ProcessCacheCommand()
 {
+    __ENTER_FUNCTION
+
     PlayerID_t PlayerID ;
 
     _MY_TRY
@@ -111,71 +120,72 @@ BOOL    DBThread::ProcessCacheCommand()
         
         Packet* pPacket = NULL ;
         
-
-        ret = RecvPacket( pPacket, PlayerID);
+        ret = RecvPacket( pPacket, PlayerID ) ;
         
     
-        if( !ret )
-            return TRUE ;
+        if ( !ret ) return TRUE ;
             
         Player* pPlayer = g_pPlayerPool->GetPlayer( PlayerID ) ;
         Assert( pPlayer ) ;
-        pPacket->Execute(pPlayer) ;
+        pPacket->Execute( pPlayer ) ;
         g_pPacketFactoryManager->RemovePacket( pPacket ) ;
             
-        return TRUE;
+        return TRUE ;
     }
 
     _MY_CATCH
     {
-        Log::SaveLog(LOGIN_LOGFILE,"DBThread::ProcessCacheCommand Get Errors,TID=%d,PlayerID =%d",getTID(),PlayerID);
-        return TRUE;
+        Log::SaveLog( LOGIN_LOGFILE, "DBThread::ProcessCacheCommand Get Errors,TID = %d, PlayerID = %d", getTID(), PlayerID ) ;
+        return TRUE ;
     }
+    
+    __LEAVE_FUNCTION
 
+        return FALSE ;
 
 }
 
-BOOL    DBThread::SendPacket(Packet* pPacket,PlayerID_t pID)
+BOOL DBThread::SendPacket( Packet* pPacket, PlayerID_t pID )
 {
     __ENTER_FUNCTION    
 
-        AutoLock_T autolock(m_Lock);
+        AutoLock_T autolock(m_Lock) ;
         
-        if(m_Input.m_pPacket == NULL)
+        if ( NULL == m_Input.m_pPacket )
         {
-            m_Input.m_pPacket    = pPacket;
-            m_Input.m_PlayerID  = pID;
-
-            return TRUE;
+            m_Input.m_pPacket    = pPacket ;
+            m_Input.m_PlayerID   = pID ;
+            return TRUE ;
         }
         else
         {
-            return FALSE;
+            return FALSE ;
         }
 
     __LEAVE_FUNCTION    
-    return FALSE;
+
+        return FALSE ;
 }
 
-BOOL    DBThread::RecvPacket( Packet*& pPacket, PlayerID_t& PlayerID)
+BOOL DBThread::RecvPacket( Packet*& pPacket, PlayerID_t& PlayerID )
 {
     __ENTER_FUNCTION
     
-        AutoLock_T autolock(m_Lock);
+        AutoLock_T autolock( m_Lock ) ;
         
-        if(m_Input.m_pPacket == NULL)
+        if ( NULL == m_Input.m_pPacket )
         {
-            return FALSE;
+            return FALSE ;
         }
         else
         {
-            pPacket        = m_Input.m_pPacket;
-            PlayerID    = m_Input.m_PlayerID;
-            m_Input.CleanUp();
-            return TRUE;
+            pPacket    = m_Input.m_pPacket ;
+            PlayerID   = m_Input.m_PlayerID ;
+            m_Input.CleanUp() ;
+            return TRUE ;
         }
 
     __LEAVE_FUNCTION
 
-        return FALSE;
+        return FALSE ;
 }
--- server/Login/DB/DBThread.h
+++ server/Login/DB/DBThread.h
@@ -1,3 +1,14 @@
+/**
+ * PAP Server Engine ( https://github.com/viticm/web-pap )
+ * $Id DBThread.h
+ * @link https://github.com/viticm/web-pap/tree/master/server for the canonical source repository
+ * @copyright Copyright (c) 2013-2013 viticm( viticm@126.com )
+ * @license
+ * @user viticm<viticm@126.com>
+ * @date 2013-9-4 16:15:30
+ * @uses the database thread for login module
+ */
+
 #ifndef _DBTHREAD_H_
 #define _DBTHREAD_H_
 
@@ -5,22 +16,22 @@
 #include "Thread.h"
 #include "Packet.h"
 
-class ODBCInterface;
+class ODBCInterface ;
 
-struct  IN_PACKET 
+struct IN_PACKET 
 {
-    Packet*            m_pPacket;
-    PlayerID_t        m_PlayerID;
+    Packet*            m_pPacket ;
+    PlayerID_t         m_PlayerID ;
 
     IN_PACKET()
     {
-        CleanUp();
+        CleanUp() ;
     }
 
-    VOID    CleanUp()
+    VOID CleanUp()
     {
-        m_pPacket    =     NULL;
-        m_PlayerID    =     INVALID_ID;
+        m_pPacket    = NULL ;
+        m_PlayerID   = INVALID_ID ;
     }
 };
 
@@ -31,38 +42,35 @@ public:
     virtual ~DBThread();
 
     //主循环
-    virtual        VOID        run();
+    virtual VOID        run();
     //停止线程执行
-    virtual VOID            stop( ) { m_Active = FALSE ; } ;
+    virtual VOID        stop() { m_Active = FALSE ; } ;
     //判断当前线程是否激活
-    BOOL                    IsActive( )
+    BOOL                IsActive()
     {
         return m_Active ; 
     } ;
 
-    BOOL                    IsConnected();
+    BOOL                IsConnected() ;
 
     //发布一个DB操作消息
-    BOOL                    SendPacket(Packet* pPacket,PlayerID_t pID);
+    BOOL                SendPacket( Packet* pPacket, PlayerID_t pID ) ;
     //读取缓存消息
-    BOOL                    RecvPacket( Packet*& pPacket, PlayerID_t& PlayerID) ;
+    BOOL                RecvPacket( Packet*& pPacket, PlayerID_t& PlayerID ) ;
     
     //初始化操作
-    BOOL                    Init();
+    BOOL                Init() ;
 
-    ODBCInterface*            GetInterface();
+    ODBCInterface*      GetInterface() ;
 private:
-    BOOL                    ProcessCacheCommand();
+    BOOL                ProcessCacheCommand() ;
 private :
     
-    BOOL                    m_Active ;
-    MyLock                    m_Lock ;
-    IN_PACKET                m_Input;
+    BOOL                m_Active ;
+    MyLock              m_Lock ;
+    IN_PACKET           m_Input ;
     //默认数据库连接
-    ODBCInterface*            mCharDBInterface;
+    ODBCInterface*      mCharDBInterface ;
 
-
-};
-
-
+} ;
 #endif
